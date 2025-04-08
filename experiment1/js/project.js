e97f34e320962706bc560fc85653502fd60c8111
RichardTao3014 <xtao10@ucsc.edu>
2025-04-07 22:06:27 -0700

    Update project.js

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -75,30 +75,3 @@ function generate() {
 
 $("#clicker").click(generate);
 generate();
-
-
-// define a class
-// class MyProjectClass {
-//   // constructor function
-//   constructor(param1, param2) {
-//     // set properties using 'this' keyword
-//     this.property1 = param1;
-//     this.property2 = param2;
-//   }
-  
-//   // define a method
-//   myMethod() {
-//     // code to run when method is called
-//   }
-// }
-
-// function main() {
-//   // create an instance of the class
-//   let myInstance = new MyProjectClass("value1", "value2");
-
-//   // call a method on the instance
-//   myInstance.myMethod();
-// }
-
-// let's get this party started - uncomment me
-//main();
